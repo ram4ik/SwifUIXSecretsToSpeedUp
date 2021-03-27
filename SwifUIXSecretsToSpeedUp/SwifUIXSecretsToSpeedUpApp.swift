f570c9c73b27965aeb60c22e41c53145a987f2c4
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-03-27 22:45:59 +0200

    Initial Commit

---
+++ SwifUIXSecretsToSpeedUp/SwifUIXSecretsToSpeedUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifUIXSecretsToSpeedUpApp.swift
+//  SwifUIXSecretsToSpeedUp
+//
+//  Created by Ramill Ibragimov on 27.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwifUIXSecretsToSpeedUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
